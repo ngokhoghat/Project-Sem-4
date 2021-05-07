27b250c55f3f8206018a783be2425c573cb9fed6
Tung Nguyen <tungnt3@smartosc.com>
2021-05-07 16:23:30 +0700

    update

---
+++ bkap/src/main/java/com/bkap/entity/Account.java
@@ -0,0 +1,10 @@
+package com.bkap.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "account")
+public class Account extends BaseEntity {
+	
+}
---
+++ bkap/src/main/java/com/bkap/entity/BaseEntity.java
@@ -0,0 +1,65 @@
+package com.bkap.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String createBy;
+	
+	@Column
+	private Date createDate;
+	
+	@Column
+	private String modifiedBy;
+	
+	@Column
+	private Date modifiedDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+}
