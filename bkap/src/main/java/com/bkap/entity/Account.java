07d4c533e65b46d023bdb99019ef8ad58724c739
Tung Nguyen <tungnt3@smartosc.com>
2021-05-11 18:13:45 +0700

    update

--- bkap/src/main/java/com/bkap/entity/Account.java
+++ bkap/src/main/java/com/bkap/entity/Account.java
@@ -1,10 +1,21 @@
 package com.bkap.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "account")
 public class Account extends BaseEntity {
-	
+	@Column(name = "displayName")
+	private String displayName;
+
+	@Column(name="email")
+	private String email;
+
+	@Column(name="password")
+	private String password;
+
+	@Column(name="roles")
+	private String roles;
 }
--- bkap/src/main/java/com/bkap/main/Application.java
+++ bkap/src/main/java/com/bkap/main/Application.java
@@ -1,11 +1,13 @@
-package com.bkap.api;
+package com.bkap.main;
 
 import org.springframework.boot.SpringApplication;
-
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-
+@EnableAutoConfiguration
+@EntityScan(basePackages = {"com.bkap.entity"}) 
 public class Application {
    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
